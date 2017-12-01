ff3ab3a847c2da746a13c45b2edc9e537d934d88
Omar  Zarate <alberich_sid@hotmail.com>
2017-12-01 13:41:34 -0600

    se agrego los cambios fisicos de la app

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>GoldFinch</title>
+        <title>Comercializacion</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -70,14 +70,14 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                        <a href="{{ url('/posts') }}">Posts</a>
-                        <a href="{{ url('/howto') }}">How to use</a>
+                        <a href="{{ url('/home') }}">Inicio</a>
+                        <a href="{{ url('/posts') }}">Atenciones</a>
+                        <a href="{{ url('/howto') }}">Como usar?</a>
                     @else
-                        <a href="{{ url('/posts') }}">Posts</a>
+                        <a href="{{ url('/posts') }}">Atenciones</a>
                         <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
-                        <a href="{{ url('/howto') }}">How to use</a>
+                        <a href="{{ url('/register') }}">Registrarse</a>
+                        <a href="{{ url('/howto') }}">Como usar?</a>
                     @endif
                 </div>
             @endif
